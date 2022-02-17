6d672a0514b632b76017aedaaaf30c430c965c36
jocelyninlay <inlay.j@gmail.com>
2022-02-17 11:54:13 -0500

    added some light styling

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function listNotes() {
          }
     }))
 }
+listNotes();
 
 form.addEventListener("submit", function () {
     event.preventDefault();
@@ -30,57 +31,86 @@ form.addEventListener("submit", function () {
     });
 });
 
+
+//listen for actions on notes list
+notesList.addEventListener('click', function (event){
+    if (event.target.classList.contains('delete')) {
+        deleteCreateNote(event.target)
+    }
+if (event.target.classList.contains('edit')) {
+    editCreateNote(event.target)
+}
+if (event.target.classList.contains('update-text')) {
+    updateCreateNote(event.target)
+}
+if (event.target.classList.contains('cancel')){
+    hideEditControls(event.target.parentElement)
+}
+})
+
+function updateCreateNote(element) {
+    const noteId = element.parentElement.id
+    const noteText = document.querySelector('.edit-text')
+    fetch(`http://localhost:3000/notes/${noteId}`, {
+        method: 'PATCH',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({
+            title: noteText.value,
+        }),
+    })
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (data) {
+            renderNoteText(element.parentElement, data)
+        })
+    }
 function renderNoteItem(noteObj) {
     const noteEl = document.createElement('li')
     noteEl.id = noteObj.id;
     noteEl.classList.add("noted");
     noteEl.innerHTML = `<span>${noteObj.title} ${noteObj.body}</span>`;
-    notesList.appendChild(noteEl)
+    renderNoteText(noteEl, noteObj)
+    notesList.prepend(noteEl)
     clearInputs()
 }
-listNotes();
+function clearInputs() {
+    form.reset()
+}
 
-// //listen for form submit
-// form.addEventListener('submit', function (event) {
-//     event.preventDefault()
-//     const notesSection = document.querySelector('#notes').value
-//     createnote(notesSection)
-// })
+function renderNoteText(noteslistItem, noteObj) {
+    noteslistItem.innerHTML = `<span class="dib w-60"><h3>${noteObj.title}</h3> <p>${noteObj.body}</p></span> <i class="fas fa-edit edit"></i> <i class="fas fa-trash delete"></i>`
+}
 
-// // //listen for actions on notes list
-// // notesList.addEventListener('clicl', function (event){
-// //     if (event.target.classList.contains('delete')) {
-// //         deletecreatenote(event.target)
-// //     }
-// // if (event.target.classList.contains('edit')) {
-// //    editcreatenote(event.target)
-// // }
-// // })
+function editCreateNote(element) {
+    showEditInput(element.parentElement)
+}
 
-// fetch(url)
-// .then(function (response) {
-//   return response.json()
-// })
-// .then(function (data) {
-//     console.log(data) 
-//     for (let noteObj of data) {
-//         renderNoteItem(noteObj);
-//     }
-// });
+function showEditInput(noteItem) {
+    noteItem.innerHTML = `
+    <input class="edit-text w-60" type="text" value="${noteItem.textContent}" autofocus>
+    <button class="update-text" data-note=${noteItem.id}>save</button>
+    <button class='cancel'>cancel</button>
+    `
+    noteItem.querySelector('input').select()
+}
 
-// function createNote(NoteText) {
-//     fetch(url, {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({
-//             title: titleText,
-//             body: NoteText,
-//         }),
-//     }) 
-//     .then((res) => res.json())
-//     .then((data) => {
-//         console.log(data)
-//         renderNoteItem(data)
-// }
-// createNote()
+function hideEditControls(noteItem) {
+    fetch(`http://localhost:3000/notes/${noteItem.id}`)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        renderNoteText(noteItem, data)
+    })
+}
+
+
+function deleteCreateNote(element) {
+    const noteId = element.parentElement.id
+    fetch(`http://localhost:3000/notes/${noteId}`, {
+        method: 'DELETE',
+    }).then(function () {
+        element.parentElement.remove()
+    })
+}
 
